7a2edbfe97e59b0df46845cacdd2f6d3e3e84705
Nilsen Strandskov <nstrands18@gmail.com>
2020-01-23 16:07:30 -0500

    created list shopping_list, created a function that takes the users input and adds it to the list and returns how many items are in the list, prints list to screen when you enter DONE

--- shopping_list.py
+++ shopping_list.py
@@ -1,3 +1,11 @@
+# Create a new empty list named shopping_list
+shopping_list = []
+# Create a function named add_to_list that declares a parameter named item
+def add_to_list(item)
+# Add the item to the list
+		shopping_list.append(item)
+		print("Added! List has {} items.".format(len(shopping_list)))
+
 def show_help():
     print("What should we pick up at the store?")
     print("""
@@ -10,7 +18,11 @@ while True:
     new_item = input("> ")
     
     if new_item == 'DONE':
+        print(shopping_list)
         break
     elif new_item == 'HELP':
         show_help()
         continue
+
+# Call add_to_list with new_item as a parameter
+		add_to_list(new_item)
